346e5d27d10e7cc9fd940336acb91c4dce011f90
MikeS96 <miguel.saaruiz@gmail.com>
2020-07-12 17:56:21 -0500

    Documented Lower bound

--- hackerrank_practice/lower_bound.cpp
+++ hackerrank_practice/lower_bound.cpp
@@ -1,3 +1,16 @@
+/**
+ *  @file lower_bound.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @brief Usage of lower bound
+ *  @version 0.1
+ *  @date 12-07-2020
+ *
+ *  You are given n integers in sorted order. Also, you are given q queries. In each query, 
+ *  you will be given an integer and you have to tell whether that integer is present in the array. 
+ *  If so, you have to tell at which index it is present and if it is not present, you have to tell 
+ *  the index at which the smallest integer that is just greater than the given number is present.
+ */
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -5,12 +18,9 @@
 #include <algorithm>
 using namespace std;
 
-/*
-You are given n integers in sorted order. Also, you are given q queries. In each query, you will be given an integer and you have to tell whether that integer is present in the array. If so, you have to tell at which index it is present and if it is not present, you have to tell the index at which the smallest integer that is just greater than the given number is present.
-*/
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+
     int n, temp;
     cin >> n;
 
