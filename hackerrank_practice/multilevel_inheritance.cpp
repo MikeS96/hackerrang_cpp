119a73216f065b340f3a5dcc2fb96aaa1778d638
MikeS96 <miguel.saaruiz@gmail.com>
2020-07-28 13:42:34 -0500

    Documented multilevel inheritance in Cpp

--- hackerrank_practice/multilevel_inheritance.cpp
+++ hackerrank_practice/multilevel_inheritance.cpp
@@ -1,3 +1,14 @@
+/**
+ *  @file multilevel_inheritance.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @brief Multi-level Inheritance in C++
+ *  @version 0.1
+ *  @date 12-07-2020
+ *
+ *  Task: Create two parent classes that inherits to a child class and use the methods from the parent classes 
+ *  in the child object.
+ */
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -8,25 +19,23 @@ using namespace std;
 class Triangle{
 	public:
 		void triangle(){
-			cout<<"I am a triangle\n";
+			cout << "I am a triangle\n";
 		}
 };
 
 class Isosceles : public Triangle{
   	public:
   		void isosceles(){
-    		cout<<"I am an isosceles triangle\n";
+    		cout << "I am an isosceles triangle\n";
   		}
 };
 
 class Equilateral : public Isosceles{
       public:
           void equilateral(){
-            cout<<"I am an equilateral triangle\n";
+            cout << "I am an equilateral triangle\n";
           }
 };
-
-//Write your code here.
 
 int main(){
   
