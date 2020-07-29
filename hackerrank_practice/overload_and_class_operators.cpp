d88aabb2192f3c19cd6ad02562f5ad62e0dd7e1e
MikeS96 <miguel.saaruiz@gmail.com>
2020-07-29 09:15:19 -0500

    Overload and Operators documented

--- hackerrank_practice/overload_and_class_operators.cpp
+++ hackerrank_practice/overload_and_class_operators.cpp
@@ -1,87 +1,89 @@
+/**
+ *  @file overload_and_class_operators.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @brief Class with overload constructor and operators
+ *  @version 0.1
+ *  @date 05-15-2020
+ *
+ *  task: Implement the class Box l,b,h are integers representing the dimensions of the box
+ *  The class should have the following functions : 
+ *  Constructors: 
+ *  Box();
+ *  Box(int,int,int);
+ *  Box(Box);
+ *  int getLength(); // Return box's length
+ *  int getBreadth (); // Return box's breadth
+ *  int getHeight ();  //Return box's height
+ *  long long CalculateVolume(); // Return the volume of the box
+ *  Overload operator < as specified
+ *  bool operator<(Box& b)
+ *  Overload operator << as specified
+ *  ostream& operator<<(ostream& out, Box& B)
+ */
+
 #include<bits/stdc++.h>
 
 using namespace std;
 
-
-
 class Box {
     private:
-    int l, b, h;
+        int l, b, h;
 
     public:
-    Box()
-    {
-        l = 0;
-        b = 0;
-        h = 0;
-    }
-    Box(int length,int breadth ,int height)
-    {
-        l = length;
-        b = breadth;
-        h = height;
-    }
-    Box(Box& B)
-    {
-        l = B.getLength();
-        b = B.getBreadth();
-        h = B.getHeight();
-    }
-    int getLength()
-    {
-        return l;
-    }
-    int getBreadth ()
-    {
-        return b;
-    }
-    int getHeight ()
-    {
-        return h;
-    }
-    long long CalculateVolume()
-    {
-        return (long long) l * b * h;
-    }
-    friend bool operator < ( Box&A,Box& B){
-        if( (A.l < B.l) || ((A.b < B.b) && (A.l == B.l)) || ((A.h < B.h) && (A.l == B.l) && (A.b == B.b)) ){
-            return true;
-        }else{
-            return false;
+        Box()
+        {
+            l = 0;
+            b = 0;
+            h = 0;
         }
-    };
-    
-    friend ostream& operator<< (ostream& output, const Box& B){
-        output << B.l << " " << B.b << " " << B.h;
-        return output;
-    }
 
+        Box(int length,int breadth ,int height)
+        {
+            l = length;
+            b = breadth;
+            h = height;
+        }
+
+        Box(Box& B)
+        {
+            l = B.getLength();
+            b = B.getBreadth();
+            h = B.getHeight();
+        }
+
+        int getLength()
+        {
+            return l;
+        }
+
+        int getBreadth ()
+        {
+            return b;
+        }
+
+        int getHeight ()
+        {
+            return h;
+        }
+
+        long long CalculateVolume()
+        {
+            return (long long) l * b * h;
+        }
+
+        friend bool operator < ( Box&A,Box& B){
+            if( (A.l < B.l) || ((A.b < B.b) && (A.l == B.l)) || ((A.h < B.h) && (A.l == B.l) && (A.b == B.b)) ){
+                return true;
+            }else{
+                return false;
+            }
+        };
+        
+        friend ostream& operator<< (ostream& output, const Box& B){
+            output << B.l << " " << B.b << " " << B.h;
+            return output;
+        }
 };
-
-//Implement the class Box  
-//l,b,h are integers representing the dimensions of the box
-
-// The class should have the following functions : 
-
-// Constructors: 
-// Box();
-// Box(int,int,int);
-// Box(Box);
-
-
-// int getLength(); // Return box's length
-// int getBreadth (); // Return box's breadth
-// int getHeight ();  //Return box's height
-// long long CalculateVolume(); // Return the volume of the box
-
-//Overload operator < as specified
-//bool operator<(Box& b)
-
-//Overload operator << as specified
-//ostream& operator<<(ostream& out, Box& B)
-
-
-
 
 void check2()
 {
