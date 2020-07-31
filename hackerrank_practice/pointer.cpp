ca18e4d38b2ae6a68119897d92e9faad68d73163
MikeS96 <miguel.saaruiz@gmail.com>
2020-07-31 11:17:49 -0500

    Documented pointers in Cpp

--- hackerrank_practice/pointer.cpp
+++ hackerrank_practice/pointer.cpp
@@ -1,10 +1,19 @@
+/**
+ *  @file pointer.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail.com)
+ *  @brief Usage of pointers
+ *  @version 0.1
+ *  @date 07-21-2020
+ *
+ *  task: In order to access the memory address of a variable, we need to prepend it with sign. E.g., &val returns the memory address of val.
+ *  This memory address is assigned to a pointer and can be shared among various functions. E.g. will assign the memory address of to pointer. 
+ *  To access the content of the memory to which the pointer points, prepend it with a *. For example, *p will return the value reflected by 
+ *  and any modification to it will be reflected at the source ().
+ */
+
 #include <stdio.h>
 #include <cstdlib>
 
-/* In order to access the memory address of a variable, , we need to prepend it with sign. E.g., &val returns the memory address of .
-
-This memory address is assigned to a pointer and can be shared among various functions. E.g. will assign the memory address of to pointer . To access the content of the memory to which the pointer points, prepend it with a *. For example, *p will return the value reflected by and any modification to it will be reflected at the source ().
-*/
 
 void update(int *a,int *b) {
     int copy_a = *a;
