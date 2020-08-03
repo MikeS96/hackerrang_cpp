bc0531cc47a90b214b38d65b823860095e5fbd79
MikeS96 <miguel.saaruiz@gmail.com>
2020-08-03 09:16:03 -0500

    Documented 'print pretty'in Cpp, This code aims to print in a fancy way different numbers

--- hackerrank_practice/print_pretty.cpp
+++ hackerrank_practice/print_pretty.cpp
@@ -1,29 +1,31 @@
+/**
+ *  @file arrays_intro.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail.com)
+ *  @brief Backwards array sorting
+ *  @version 0.1
+ *  @date 05-15-2020
+ *
+ *  task: Strip its decimal (i.e., truncate it) and print its hexadecimal representation (including the
+ *  prefix) in lower case letters. Print it to a scale of decimal places, preceded by a or sign 
+ *  (indicating if it's positive or negative), right justified, and left-padded with underscores so that 
+ *  the printed result is exactly characters wide.
+ * 
+ *  Print it to a scale of exactly nine decimal places, expressed in scientific notation using upper case.
+ *  Sample Input
+ * 
+ * 1
+ * 100.345 2006.008 2331.41592653498
+ * 
+ * Sample Output
+ * 
+ * 0x64 
+ * _______+2006.01  
+ * 2.331415927E+03
+ */
+
 #include <iostream>
 #include <iomanip> 
 using namespace std;
-
-/*
-
-: Strip its decimal (i.e., truncate it) and print its hexadecimal representation (including the
-prefix) in lower case letters.
-
-: Print it to a scale of decimal places, preceded by a or sign (indicating if it's positive or negative), right justified, and left-padded with underscores so that the printed result is exactly
-characters wide.
-
-: Print it to a scale of exactly nine decimal places, expressed in scientific notation using upper case.
-
-Sample Input
-
-1  
-100.345 2006.008 2331.41592653498
-
-Sample Output
-
-0x64             
-_______+2006.01  
-2.331415927E+03
-
-*/
 
 int main() {
 	int T; cin >> T;
