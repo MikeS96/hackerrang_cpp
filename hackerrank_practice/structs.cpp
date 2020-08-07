37aa6b393713ff0093b626c6d5d45d77ca0ee5b4
MikeS96 <miguel.saaruiz@gmail.com>
2020-08-07 09:13:38 -0500

    Documented Structs in C++

--- hackerrank_practice/structs.cpp
+++ hackerrank_practice/structs.cpp
@@ -1,12 +1,24 @@
+/**
+ *  @file structs.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail.com)
+ *  @brief Struct Student
+ *  @version 0.1
+ *  @date 08-07-2020
+ *
+ *  task: struct is a way to combine multiple fields to represent a
+ *  composite data structure, which further lays the foundation 
+ *  for Object Oriented Programming. 
+ *  For example, we can store details related to a student in a struct 
+ *  consisting of his age (int), first_name (string), last_name (string)
+ *  and standard (int).
+ */
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-/* struct is a way to combine multiple fields to represent a composite data structure, which further lays the foundation for Object Oriented Programming. For example, we can store details related to a student in a struct consisting of his age (int), first_name (string), last_name (string) and standard (int).
-*/
 
 struct Student {
     int age;
