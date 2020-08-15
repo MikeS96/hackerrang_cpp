cf99e1766b1facbfb7eb0284865bbbcde8f48942
MikeS96 <miguel.saaruiz@gmail.com>
2020-08-15 09:24:35 -0500

    Documented how to Throw multiple Exceptions in C++

--- hackerrank_practice/throw_multiple_exceptions.cpp
+++ hackerrank_practice/throw_multiple_exceptions.cpp
@@ -1,3 +1,22 @@
+/**
+ *  @file throw_multiple_exceptions.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail.com)
+ *  @brief Creating multiple exceptions
+ *  @version 0.1
+ *  @date 05-15-2020
+ *
+ *  task: Your friend set up a small computational server that performs complex calculations.
+ *  It has a function that takes 5 large numbers as its input and returns a numeric result. 
+ *  Unfortunately, there are various exceptions that may occur during execution.
+ *  Complete the code in your editor so that it prints appropriate error messages, should anything go wrong. 
+ *  The expected behavior is defined as follows:
+ * 
+ * If the compute function runs fine with the given arguments, then print the result of  the function call.
+ * If it fails to allocate the memory that it needs, print Not enough memory.
+ * If any other standard C++ exception occurs, print Exception: S where Sis the exception's error message.
+ * If any non-standard exception occurs, print Other Exception.
+ */
+
 #include <iostream>
 #include <exception>
 #include <string>
@@ -6,19 +25,6 @@
 #include <cmath>
 using namespace std;
 
-/*
-
-Your friend set up a small computational server that performs complex calculations.
-It has a function that takes 5 large numbers as its input and returns a numeric result. Unfortunately, there are various exceptions that may occur during execution.
-
-Complete the code in your editor so that it prints appropriate error messages, should anything go wrong. The expected behavior is defined as follows:
-
-    If the compute function runs fine with the given arguments, then print the result of  the function call.
-    If it fails to allocate the memory that it needs, print Not enough memory.
-    If any other standard C++ exception occurs, print Exception: S where Sis the exception's error message.
-    If any non-standard exception occurs, print Other Exception.
-
-*/
 class Server {
 private:
 	static int load;
