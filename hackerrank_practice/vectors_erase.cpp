c2806d9b9e9fce618c0134571a8f08b913e5f259
MikeS96 <miguel.saaruiz@gmail.com>
2020-08-25 14:29:32 -0500

    Documented how to erase values in a vector

--- hackerrank_practice/vectors_erase.cpp
+++ hackerrank_practice/vectors_erase.cpp
@@ -1,3 +1,19 @@
+/**
+ *  @file vectors_erase.cpp
+ *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @brief Removing values in a range
+ *  @version 0.1
+ *  @date 07-25-2020
+ *
+ *  task: You are provided with a vector of N integers. Then, you are given 2 queries. For the 
+ *  first query, you are provided with 1 integer, which denotes a position in the vector. The value 
+ *  at this position in the vector needs to be erased. The next query consists of 2 integers 
+ *  denoting a range of the positions in the vector. The elements which fall under that range 
+ *  should be removed. The second query is performed on the updated vector which we get after 
+ *  performing the first query.
+ */
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -5,11 +21,8 @@
 #include <algorithm>
 using namespace std;
 
-/* 
-You are provided with a vector of N integers. Then, you are given 2 queries. For the first query, you are provided with 1 integer, which denotes a position in the vector. The value at this position in the vector needs to be erased. The next query consists of 2 integers denoting a range of the positions in the vector. The elements which fall under that range should be removed. The second query is performed on the updated vector which we get after performing the first query.
-*/ 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+
     int n; // Number of elements in the vector
     cin >> n;
 
