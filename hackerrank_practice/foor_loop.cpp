88556cd5158436d0491df74a462c49f2ad297f00
MikeS96 <miguel.saaruiz@gmail.com>
2020-10-14 17:13:25 -0500

    Updated the description of foor_loop.cpp

--- hackerrank_practice/foor_loop.cpp
+++ hackerrank_practice/foor_loop.cpp
@@ -1,6 +1,6 @@
 /**
  *  @file for_loop.cpp
- *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @author Miguel Saavedra (miguel.saavedra@uao.edu.co)
  *  @brief For loop to identify typed number
  *  @version 0.1
  *  @date 05-30-2020
