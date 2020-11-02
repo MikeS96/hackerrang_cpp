54b484ff67c7bf890ea1cd624580f9a6df804d02
MikeS96 <miguel.saaruiz@gmail.com>
2020-11-02 09:36:41 -0500

    Updated the description of maps.cpp

--- hackerrank_practice/maps.cpp
+++ hackerrank_practice/maps.cpp
@@ -1,6 +1,6 @@
 /**
  *  @file maps.cpp
- *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @author Miguel Saavedra (miguel.saavedra@uao.edu.co)
  *  @brief Using maps in C++
  *  @version 0.1
  *  @date 05-30-2020
