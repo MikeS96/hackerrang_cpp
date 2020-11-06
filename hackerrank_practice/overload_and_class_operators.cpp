a1a8816d93d3c4d813b29076fdc0822d234f1c89
MikeS96 <miguel.saaruiz@gmail.com>
2020-11-06 10:28:07 -0500

    Updated the header's description of overload_and_class_operators.cpp

--- hackerrank_practice/overload_and_class_operators.cpp
+++ hackerrank_practice/overload_and_class_operators.cpp
@@ -1,6 +1,6 @@
 /**
  *  @file overload_and_class_operators.cpp
- *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @author Miguel Saavedra (miguel.saavedra@uao.edu.co)
  *  @brief Class with overload constructor and operators
  *  @version 0.1
  *  @date 05-15-2020
