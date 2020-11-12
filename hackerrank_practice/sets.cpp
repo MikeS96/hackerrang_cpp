a1c86e1a1e2dbc1f60d9d09267ad0630f3e831fc
MikeS96 <miguel.saaruiz@gmail.com>
2020-11-12 15:30:30 -0500

    Updated the header's description of sets.cpp

--- hackerrank_practice/sets.cpp
+++ hackerrank_practice/sets.cpp
@@ -1,6 +1,6 @@
 /**
  *  @file sets.cpp
- *  @author Miguel Saavedra (miguel.saaruiz@gmail@gmail.com)
+ *  @author Miguel Saavedra (miguel.saavedra@uao.edu.co)
  *  @brief Usage of sets method in C++
  *  @version 0.1
  *  @date 07-04-2020
