d2456aa12e37f098ca1f5995372edd6334ef7cd3
MikeS96 <miguel.saaruiz@gmail.com>
2020-12-08 17:00:42 -0500

    Added header's description to const.cpp

--- Cpp_course/week1_basics/const.cpp
+++ Cpp_course/week1_basics/const.cpp
@@ -1,4 +1,11 @@
-/*Goal: use constant variables*/
+/**
+ *  @file const.cpp
+ *  @author Miguel Saavedra (miguel.saavedra@uao.edu.co)
+ *  @brief Create constant variables
+ *  @version 0.1
+ *  @date 12-08-2020
+ *
+ */
 
 #include <iostream>
 using namespace std;
