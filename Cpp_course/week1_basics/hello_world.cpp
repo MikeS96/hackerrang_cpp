b608ce68219b5466589364ba2c1d6943f6d51af6
MikeS96 <miguel.saaruiz@gmail.com>
2020-12-14 08:14:37 -0500

    Added header's description to hello_world.cpp

--- Cpp_course/week1_basics/hello_world.cpp
+++ Cpp_course/week1_basics/hello_world.cpp
@@ -1,9 +1,15 @@
-/*write a C++ program that outputs the following statement:
- *** "Hello world, I am ready for C++"
+/**
+ *  @file hello_world.cpp
+ *  @author Miguel Saavedra (miguel.saavedra@uao.edu.co)
+ *  @brief Simple Hello world
+ *  @version 0.1
+ *  @date 12-14-2020
+ *
  */
- #include <iostream>
 
- int main() {
-     std::cout << "Hello world, I am ready for C++";
-     return 0;
- }
+#include <iostream>
+
+int main() {
+    std::cout << "Hello world, I am ready for C++";
+    return 0;
+}
