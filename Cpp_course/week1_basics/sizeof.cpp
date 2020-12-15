d6287c436cf6378d7342b7b25493bdfef940c781
MikeS96 <miguel.saaruiz@gmail.com>
2020-12-15 13:29:17 -0500

    Updated the header's description of sizeof.cpp

--- Cpp_course/week1_basics/sizeof.cpp
+++ Cpp_course/week1_basics/sizeof.cpp
@@ -1,11 +1,10 @@
-/*GOAL: Practice writing to the console and learn 
-**the variables types available in C++
-**Print the sizes of each variable to the console.
-**Print them in the following order:
-**int, short, long, char, float, double, bool
-**
-**Use the command sizeof(variable type) ie: sizeof(int)
-*/
+/**
+ *  @file sizeof.cpp
+ *  @author Miguel Saavedra (miguel.saavedra@uao.edu.co)
+ *  @brief Practice writing to the console and learn c++ variables
+ *  @version 0.1
+ *  @date 12-15-2020
+ */
 
 #include "iostream"
 
